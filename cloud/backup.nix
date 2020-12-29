6596879d6fbe1b92c324943c141c71c0dbca1a1b
ZerataX <mail@zera.tax>
2020-12-29 18:21:38 +0100

    cloud: supply ssh-key

--- cloud/backup.nix
+++ cloud/backup.nix
@@ -1,9 +1,10 @@
 { pkgs, config, lib, ...}:
 
 let
-  ssh-fingerprint = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEA5K62E/ZFLEOIQmzKClxVAP5GmR+6ir+hWxPxK9XfvMZtTtCcnhXBnXNfQlSrX301INy9DiVfN+bRYHS3LU7TUfEcd6E5iwCOH6o9nRVZS7IkJDN/cw0m3co7cFeoayNZylIeACVfM7DwBjzzOXMV3T4hN5LbHkpv63CNTTTQqBaak+CZBQFmzMgIYGiEAi5a3yzZFpVh46JkaasDO2C9SfTNBIuCfaUIAbMbXb09B6FsirBdhndEI2fpT+1jYM0PUeqnxDbYuv5UDwDgKADo/HBAid1X4srJZzMjcnFjtwrazk3/DzyICnZM4R6xuw4cOYiDgfbfYsLYaT70YqFPUw== kaine@gestalt";
+  ssh-fingerprint = builtins.readFile ./id_ed25519.pub;
 in {
   deployment.keys.aws-secrets.text = builtins.readFile ./aws-secrets.key;
+  deployment.keys.ssh-key.text = builtins.readFile ./id_ed25519.key;
 
   systemd.services.dbBackup = {
     serviceConfig = {
@@ -13,9 +14,8 @@ in {
     };
     script = ''
     today=$(date +"%Y%m%d")
-    expire=$(date -d "06:45 today + 30 days" --utc +'%Y-%m-%dT%H:%M:%SZ')
+    expire=$(date -d "02:30 today + 30 days" --utc +'%Y-%m-%dT%H:%M:%SZ')
     ${pkgs.postgresql_11}/bin/pg_dump \
-      --format=c \
       -U postgres \
       -Fc nextcloud | \
     ${pkgs.age}/bin/age \
@@ -32,5 +32,29 @@ in {
     partOf = [ "dbBackup.service" ];
     timerConfig.OnCalendar = "*-*-* 2:30:00";
     timerConfig.Persistent = true;
+  };
+
+  systemd.services."restoreDbBackup@" = {
+    environment = {
+      BACKUP_DATE = "%i";
+    };
+    serviceConfig = {
+      EnvironmentFile = config.deployment.keys.aws-secrets.path;
+      User = "postgres";
+      Type = "oneshot";
+    };
+    serviceConfig.Type = "oneshot";
+    script = ''
+      ${pkgs.awscli}/bin/aws s3 cp \
+        "s3://$AWS_BACKUP_BUCKET/cloud-backup.$BACKUP_DATE.dump" \
+        - | \
+      ${pkgs.age}/bin/age \
+        --decrypt \
+        -i "${config.deployment.keys.ssh-key.path}" | \
+      ${pkgs.postgresql_11}/bin/pg_restore \
+        -U postgres
+        --clean \
+        --dbname nextcloud
+    '';
   };
 }
