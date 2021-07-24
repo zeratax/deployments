266608385b5851b1f44a4e443a582273cb037a56
ZerataX <mail@zera.tax>
2021-07-24 20:47:40 +0200

    gestalt: let cachix manage itself

--- gestalt/cachix.nix
+++ gestalt/cachix.nix
@@ -1,8 +1,7 @@
-# WARN: this file will get overwritten by $ cachix use <name>
 { pkgs, lib, ... }:
 
 let
-  folder = ./cachix;
+  folder = /etc/nixos/cachix;
   toImport = name: value: folder + ("/" + name);
   filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
   imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
