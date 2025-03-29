d1ebd76200e0de85c76fb6b80f782157860b37a8
ZerataX <mail@zera.tax>
2025-03-29 22:18:19 +0000

    cloud: nextcloud 28 -> 31

--- cloud/nextcloud.nix
+++ cloud/nextcloud.nix
@@ -64,7 +64,18 @@
     https = true;
 
     extraApps = with config.services.nextcloud.package.packages.apps; {
-      inherit calendar contacts maps previewgenerator tasks twofactor_nextcloud_notification twofactor_webauthn;
+      inherit
+        calendar
+        contacts
+        # files_markdown
+        previewgenerator
+        # maps
+        music
+        news
+        tasks
+        twofactor_webauthn
+        # unsplash
+        ;
       # generate with https://github.com/NixOS/nixpkgs/tree/master/pkgs/servers/nextcloud/packages
       # checksum = pkgs.fetchNextcloudApp {
       # broken in 28, see https://github.com/westberliner/checksum/issues/86
@@ -72,49 +83,10 @@
       #   sha256 = "sha256-BOKvJEjF1dLChd9LcpfXC0enrmuL1CJW1OIGOzH2AzQ=";
       #   license = "gpl3Plus";
       # };
-      # files_markdown = pkgs.fetchNextcloudApp {
-      # broken in 28, https://github.com/icewind1991/files_markdown/issues/218
-      #   url = "https://github.com/icewind1991/files_markdown/releases/download/v2.4.1/files_markdown-v2.4.1.tar.gz";
-      #   sha256 = "0p97ha6x3czzbflavmjn4jmz3z706h5f84spg4j7dwq3nc9bqrf7";
-      #   license = "agpl3Plus";
-      # };
-      # maps = pkgs.fetchNextcloudApp {
-      #   url = "https://github.com/nextcloud/maps/releases/download/v1.1.0-2a-nightly/maps-1.1.0-2a-nightly.tar.gz";
-      #   sha256 = "0517kakkk7lr7ays6rrnl276709kcm5yvkp8g6cwjnfih7pmnkn9";
-      #   license = "agpl3Plus";
-      # };
-      # music = pkgs.fetchNextcloudApp {
-      # broken in 28, see https://github.com/owncloud/music/issues/1112
-      #   url = "https://github.com/owncloud/music/releases/download/v1.9.1/music_1.9.1_for_nextcloud.tar.gz";
-      #   sha256 = "06w82v34csx4scl5n4k4fpdxiivrzjb3yvj3hh4bc15gdz68cis9";
-      #   license = "agpl3Plus";
-      # };
-      news = pkgs.fetchNextcloudApp {
-        url = "https://github.com/nextcloud/news/releases/download/25.0.0-alpha3/news.tar.gz";
-        sha256 = "sha256-AENBJH/bEob5JQvw4WEi864mdLYJ5Mqe78HJH6ceCpI=";
-        license = "agpl3Plus";
-      };
-      # previewgenerator = pkgs.fetchNextcloudApp {
-      #   url = "https://github.com/nextcloud-releases/previewgenerator/releases/download/v5.3.0/previewgenerator-v5.3.0.tar.gz";
-      #   sha256 = "0ziyl7kqgivk9xvkd12byps6bb3fvcvdgprfa9ffy1zrgpl9syhk";
-      #   license = "agpl3Plus";
-      # };
-      twofactor_totp = pkgs.fetchNextcloudApp {
-        # not available for 28 so we have to define it ourselves
-        url = "https://github.com/nextcloud-releases/twofactor_totp/releases/download/v6.4.1/twofactor_totp-v6.4.1.tar.gz";
-        sha256 = "189cwq78dqanqxhsl69dahdkh230zhz2r285lvf0b7pg0sxcs0yc";
-        license = "agpl3Plus";
-      };
-      # unsplash = pkgs.fetchNextcloudApp {
-      # broken in 28, see https://github.com/nextcloud/unsplash/issues/131
-      #   url = "https://github.com/nextcloud/unsplash/releases/download/v2.2.1/unsplash.tar.gz";
-      #   sha256 = "1ya1h4nb9cyj1hdgb5l5isx7a43a7ri92cm0h8nwih20hi6a9wzx";
-      #   license = "agpl3Plus";
-      # };
     };
     extraAppsEnable = true;
 
-    package = pkgs.nextcloud28;
+    package = pkgs.nextcloud31;
 
     maxUploadSize = "10G";
 
