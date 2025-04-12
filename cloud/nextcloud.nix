20016dade7f3d961f8612775ee085d85c3fb5235
ZerataX <mail@zera.tax>
2025-04-12 14:23:21 +0000

    cloud: update preview providers

--- cloud/nextcloud.nix
+++ cloud/nextcloud.nix
@@ -108,6 +108,20 @@
       defaultPhoneRegion = "DE";
     };
 
+    settings.enabledPreviewProviders = [
+      "OC\\Preview\\BMP"
+      "OC\\Preview\\GIF"
+      "OC\\Preview\\JPEG"
+      "OC\\Preview\\Krita"
+      "OC\\Preview\\MarkDown"
+      "OC\\Preview\\MP3"
+      "OC\\Preview\\OpenDocument"
+      "OC\\Preview\\PNG"
+      "OC\\Preview\\TXT"
+      "OC\\Preview\\XBitmap"
+      "OC\\Preview\\HEIC"
+    ];
+
     phpOptions."opcache.interned_strings_buffer" = "23";
 
     poolSettings = {
